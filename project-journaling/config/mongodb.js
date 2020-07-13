a6ce18e46a22e59de0c782fedb855345478dd7e1
Alice Prigent <aliprigent@gmail.com>
2020-07-13 12:42:09 +0200

    Configured Database + models folder + views

---
+++ project-journaling/config/mongodb.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then((x) =>
+    console.log(`Connected to Mongo ---> Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
