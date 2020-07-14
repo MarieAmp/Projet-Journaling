2154070b5667af1e363b4bb6a303f9ece1fa87c1
Cha Noguer <definitely.not.a.chatbot@gmail.com>
2020-07-14 19:43:48 +0200

    added questions seeding with 79questions

--- project-journaling/models/Question.js
+++ project-journaling/models/Question.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const questionSchema = new Schema({
   question: String,
-  theme: { type: String, enum: ['relationships', 'happiness', 'work'] }, 
+  theme: { type: String, enum: ['relationships', 'personality', 'lifestyle', 'work', 'deep'] }, 
 })
 
 const QuestionModel = mongoose.model("Question", questionSchema);
