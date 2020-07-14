3f47309558ed2ad9c1bd7609c77f6534c259cb36
MarieAmp <marymary09@hotmail.com>
2020-07-14 12:17:48 +0200

    quote en galère

--- project-journaling/public/javascripts/inspirational.js
+++ project-journaling/public/javascripts/inspirational.js
@@ -1,6 +1,6 @@
 const inspiContainer=document.getElementById("quote-container");
 const btnQuote=document.getElementById("get-quote");
-
+g
 function getQuote() {
     axios.get(
         "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
