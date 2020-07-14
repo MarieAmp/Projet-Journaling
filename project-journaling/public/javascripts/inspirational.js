177f9997e37e7793576aaf6157b16eb54403b9f7
MarieAmp <marymary09@hotmail.com>
2020-07-14 16:54:24 +0200

    quote on dashboard

--- project-journaling/public/javascripts/inspirational.js
+++ project-journaling/public/javascripts/inspirational.js
@@ -5,8 +5,11 @@ const btnQuote=document.getElementById("get-quote");
 function displayQuote() {
     axios.get("/dashboard/quote")
     .then((quote)=>{
-        console.log(quote.data.quoteText);
-        inspiContainer.innerHTML+= `<p>${quote.data.quoteText}</p>`
+        console.log(quote.data);
+        inspiContainer.innerHTML+= `<p>${quote.data.quoteText}</p>
+        <br>.
+        <p>${quote.data.quoteAuthor}</p><br>
+        <a href="${quote.data.quoteLink}">source:forismatic.com<a>`
     })
     .catch((err)=>console.log(err))
   }
