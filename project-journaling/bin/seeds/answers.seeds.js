3a73bf2369e70a2ae0799a9c9befd29fed629f4f
MarieAmp <marymary09@hotmail.com>
2020-07-17 13:26:11 +0200

    answers seeds ok

--- project-journaling/bin/seeds/answers.seeds.js
+++ project-journaling/bin/seeds/answers.seeds.js
@@ -5,56 +5,112 @@ const answerModel = require("../../models/Answers");
 const responseBase = [
    {
    response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
-   date:2020/07/01,
+   date:'2020-01-12',
    mood:"Good",
    id_user:"5f1175904fac51768cf6bd82",
    id_question:"5f0ea3f990e6fef2e8e2c581"
 },
 {
    response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
-   date:2020/07/02,
+   date:'2020-02-13',
    mood:"Good",
    id_user:"5f1175904fac51768cf6bd82",
    id_question:"5f0ea3f990e6fef2e8e2c575"
 },
 {
    response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
-   date:2020/07/03,
+   date:'2020-01-14',
    mood:"Good",
    id_user:"5f1175904fac51768cf6bd82",
    id_question:"5f0ea3f990e6fef2e8e2c555"
 },
 {
    response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
-   date:2020/07/04,
+   date:'2020-01-15',
    mood:"Good",
    id_user:"5f1175904fac51768cf6bd82",
    id_question:"5f0ea3f990e6fef2e8e2c575"
 },
 {
    response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
-   date:2020/07/05,
+   date:'2019-07-05',
    mood:"Good",
    id_user:"5f1175904fac51768cf6bd82",
    id_question:"5f0ea3f990e6fef2e8e2c55c"
 },
 {
    response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
-   date:2020/07/06,
+   date:'2019-07-06',
    mood:"Good",
    id_user:"5f1175904fac51768cf6bd82",
    id_question:"5f0ea3f990e6fef2e8e2c56a"
 },
 {
    response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
-   date:2020/02/03,
+   date:'2019-02-03',
    mood:"Bad",
    id_user:"5f1175904fac51768cf6bd82",
    id_question:"5f0ea3f990e6fef2e8e2c575"
 },
 {
    response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
-   date:2020/02/01,
+   date:'2019-02-01',
+   mood:"Neutral",
+   id_user:"5f1175904fac51768cf6bd82",
+   id_question:"5f0ea3f990e6fef2e8e2c56a"
+},
+{
+   response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
+   date:'2020-07-12',
+   mood:"Good",
+   id_user:"5f1175904fac51768cf6bd82",
+   id_question:"5f0ea3f990e6fef2e8e2c581"
+},
+{
+   response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
+   date:'2020-07-13',
+   mood:"Good",
+   id_user:"5f1175904fac51768cf6bd82",
+   id_question:"5f0ea3f990e6fef2e8e2c575"
+},
+{
+   response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
+   date:'2020-07-14',
+   mood:"Good",
+   id_user:"5f1175904fac51768cf6bd82",
+   id_question:"5f0ea3f990e6fef2e8e2c555"
+},
+{
+   response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
+   date:'2020-07-15',
+   mood:"Good",
+   id_user:"5f1175904fac51768cf6bd82",
+   id_question:"5f0ea3f990e6fef2e8e2c575"
+},
+{
+   response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
+   date:'2020-07-05',
+   mood:"Good",
+   id_user:"5f1175904fac51768cf6bd82",
+   id_question:"5f0ea3f990e6fef2e8e2c55c"
+},
+{
+   response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
+   date:'2020-07-06',
+   mood:"Good",
+   id_user:"5f1175904fac51768cf6bd82",
+   id_question:"5f0ea3f990e6fef2e8e2c56a"
+},
+{
+   response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
+   date:'2020-02-03',
+   mood:"Bad",
+   id_user:"5f1175904fac51768cf6bd82",
+   id_question:"5f0ea3f990e6fef2e8e2c575"
+},
+{
+   response : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio, unde reiciendis inventore similique quibusdam consequuntur dolorem accusamus, nostrum explicabo laboriosam? Excepturi laboriosam, molestiae saepe fugit quisquam distinctio omnis repellendus!",
+   date:'2020-02-01',
    mood:"Neutral",
    id_user:"5f1175904fac51768cf6bd82",
    id_question:"5f0ea3f990e6fef2e8e2c56a"
@@ -64,6 +120,6 @@ const responseBase = [
 
 
 
-answerModel.create(responseBase)
+answerModel.insertMany(responseBase)
 .then(dbRes => console.log(dbRes))
 .catch(dbErr => console.log(dbErr));
